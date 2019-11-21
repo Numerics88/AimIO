0a71f9b964c0c175f9927a349ab60a6d320260dd
Bryce Besler <babesler@ucalgary.ca>
2019-11-20 20:10:52 -0700

    Add compiler to build of meta.yaml

--- conda-recipe/AimIO/build.sh
+++ conda-recipe/AimIO/build.sh
@@ -5,6 +5,18 @@ mkdir build
 cd build
 BUILD_CONFIG=Release
 
+# Platform specifics
+declare -a CMAKE_PLATFORM_FLAGS
+case $(uname | tr '[:upper:]' '[:lower:]') in
+  linux*)
+    ;;
+  darwin*)
+		# Get the SDK
+		CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
+    ;;
+  *)
+esac
+
 # CMake
 cmake .. \
 	-G "Ninja" \
@@ -14,7 +26,8 @@ cmake .. \
 	-DCMAKE_INSTALL_RPATH:PATH="${PREFIX}/lib" \
 	-DBOOST_ROOT:PATH="${PREFIX}" \
 	-DBUILD_SHARED_LIBS:BOOL=OFF \
-	-DENABLE_TESTING:BOOL=ON
+	-DENABLE_TESTING:BOOL=ON \
+	"${CMAKE_PLATFORM_FLAGS[@]}"
 
 # Compile and install
 ninja install -v
