f3693046b999494df22e4add5cc4b1ad915c256c
friedchickean <kdarg29@gmail.com>
2025-05-07 11:44:25 -0600

    Test deployment to conda

--- conda-recipe/AimIO/build.sh
+++ conda-recipe/AimIO/build.sh
@@ -1,5 +1,9 @@
 set -x
 
+# Deactivate any existing conda environment to avoid mismatches with build config
+# Without this, the build fails for MacOS-13 runner
+conda deactivate
+
 # Create build directory
 mkdir build
 cd build
@@ -37,3 +41,5 @@ ninja install -v
 
 # Run tests
 ctest -V
+
+conda activate
